0ca0bc17f5d6e78c9a0fe21c1f6e893e7da7c4df
q1q1w2w2 <xmrrhdwjdqls@gmail.com>
2025-01-17 22:52:44 +0900

    Refactor: 스케줄러 실행 시간 수정

--- src/main/java/com/ticketing/project/service/ConcertScheduler.java
+++ src/main/java/com/ticketing/project/service/ConcertScheduler.java
@@ -4,8 +4,10 @@ import com.ticketing.project.entity.Concert;
 import com.ticketing.project.repository.ConcertRepository;
 import com.ticketing.project.util.enums.ConcertStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -15,13 +17,17 @@ import static com.ticketing.project.util.enums.ConcertStatus.*;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ConcertScheduler {
 
     private final ConcertRepository concertRepository;
 
-    @Scheduled(fixedRate = 60000)
+    @Transactional
+    @Scheduled(cron = "0 * * * * *")
     public void updateConcertStatus() {
         LocalDateTime now = LocalDateTime.now();
+        log.info("현재 시각: {}", now);
+
         List<Concert> concerts = concertRepository.findAllByStatusNotIn(
                 Arrays.asList(CANCELLED, RESERVATION_CLOSED)
         );
